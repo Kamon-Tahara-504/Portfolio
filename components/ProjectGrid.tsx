53765a14836c4960bea310995d5d3439ff75494f
Kqn0n <taharacomeon.0504@gmail.com>
2026-04-06 23:21:18 +0900

    change: Projectカードの表示挙動とグリッド構成を調整

--- components/ProjectGrid.tsx
+++ components/ProjectGrid.tsx
@@ -94,7 +94,7 @@ export default function ProjectGrid({
   }
 
   return (
-    <div className="grid grid-cols-2 gap-4 md:gap-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-8">
       {filteredProjects.map((project, index) => (
         <ProjectCard
           key={project.id}
